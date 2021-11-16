0a2f477f60d05543ad1e3e3223e50901bf171c1f
Lvzoey <lzhzoey@gmail.com>
2021-11-16 15:52:43 -0500

    create

---
+++ src/main/java/quizdesigner/QuizDesignerApplication.java
@@ -0,0 +1,13 @@
+package quizdesigner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizDesignerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizDesignerApplication.class, args);
+    }
+
+}
